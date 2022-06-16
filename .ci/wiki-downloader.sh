7ba0d26d5efefae0a7b383889bce543004403e6f
Matthew Kilgore <mattkilgore12@gmail.com>
2022-06-15 22:37:10 -0400

    wiki stuff

---
+++ .ci/wiki-downloader.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+base="http://qb64phoenix.com/qb64wiki/"
+allPagesApi="api.php?action=query&list=allpages&aplimit=max&format=json"
+getPageContext="api.php?action=parse&prop=wikitext&formatversion=2&format=json"
+
+jsonResponse=$(curl -L "$base$allPagesApi")
+
+echo "$jsonResponse"
+
+echo "$jsonResponse" | jq -c ".query.allpages[]" | while read line
+do
+    pageid=$(echo "$line" | jq -r ".pageid")
+    pageTitle=$(echo "$line" | jq -r ".title")
+
+    if [ "$pageTitle" == '*' ]; then
+        continue;
+    fi
+
+    if [ "$pageTitle" == '/' ]; then
+        continue;
+    fi
+
+    if [ "$pageTitle" == '\' ]; then
+        continue;
+    fi
+
+    echo "PageID: $pageid"
+    echo "PageTitle: $pageTitle"
+
+    curl -L "$base$getPageContext&pageid=$pageid" | \
+        jq -r ".parse.wikitext" | \
+        sed 's/\\n/\n/g' > "./wiki/$pageTitle.mediawiki" &
+
+    # page=$(curl -L "$base$getPageContext&oldid=$pageid") # > "./wiki/$pageTitle"
+    # wikitext=$(echo "$page" | jq -r ".parse.wikitext" | sed 's/\\n/\n/g')
+
+    # echo "$wikitext" > "./wiki/$pageTitle"
+done
+
+wait
+
+cat "./wiki/\$ASSERTS.mediawiki"
+
+cd ./wiki
+
+git config --local user.email "41898282+github-actions[bot]@users.noreply.github.com"
+git config --local user.name "github-actions[bot]"
+git add .
+
+if ! git diff --cached --quiet; then
+    echo "Pushing updated wiki"
+    git commit -m 'Automatic update of Wiki'
+    git push
+else
+    echo "No changes to wiki"
+fi
