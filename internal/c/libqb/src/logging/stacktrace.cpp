9e3fe0af62aa30037d9f7ed171eb53c98551e640
Matthew Kilgore <mattkilgore12@gmail.com>
2024-08-18 01:15:26 -0400

    Fix stacktrace log function exclusion

--- internal/c/libqb/src/logging/stacktrace.cpp
+++ internal/c/libqb/src/logging/stacktrace.cpp
@@ -9,11 +9,11 @@
 
 struct stacktrace_state {
     int frame;
+    int skipped;
     std::string str;
 };
 
-static _Unwind_Reason_Code handler(struct _Unwind_Context* context, void* ref)
-{
+static _Unwind_Reason_Code handler(struct _Unwind_Context *context, void *ref) {
     stacktrace_state *result = static_cast<stacktrace_state *>(ref);
 
 	if (result->frame > 40)
@@ -21,16 +21,22 @@ static _Unwind_Reason_Code handler(struct _Unwind_Context* context, void* ref)
 
     result->frame++;
 
-    // Skip the libqb_log(), libqb_vlog(), and libqb_log_get_stacktrace()
-    // entries in the stack
-    if (result->frame < 4)
-        return _URC_NO_REASON;
-
     const void *addr = (const void *)_Unwind_GetIP(context);
     if (!addr)
         return _URC_NORMAL_STOP;
 
     std::string symbol = libqb_log_resolve_symbol(addr);
+
+    // Remove these symbols from the stacktrace
+    if (symbol == "libqb_log"
+        || symbol == "libqb_vlog"
+        || symbol == "libqb_log_qbs"
+        || symbol == "libqb_log_get_stacktrace") {
+
+        result->skipped++;
+        return _URC_NO_REASON;
+    }
+
 
     std::optional<std::string> qb64_symbol = libqb_log_resolve_qb64_symbol(symbol.c_str());
 
@@ -38,7 +44,7 @@ static _Unwind_Reason_Code handler(struct _Unwind_Context* context, void* ref)
         symbol = *qb64_symbol;
 
     char line[200];
-    snprintf(line, sizeof(line), "#%-2d [0x%p] in %s\n", result->frame - 3, addr, symbol.c_str());
+    snprintf(line, sizeof(line), "#%-2d [0x%p] in %s\n", result->frame - result->skipped, addr, symbol.c_str());
 
     result->str += line;
 	return _URC_NO_REASON;
@@ -47,6 +53,7 @@ static _Unwind_Reason_Code handler(struct _Unwind_Context* context, void* ref)
 std::string libqb_log_get_stacktrace() {
     stacktrace_state state = {
         .frame = 0,
+        .skipped = 0,
         .str = "",
     };
 
